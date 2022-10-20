8498dc9c6c3738265175d149f885e8679c3456db
KarinSV <karina.svanstrom@gmail.com>
2022-10-20 09:48:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const randomNumber = Math.floor(Math.random() * 10) + 1
+    console.log('Random Number', randomNumber)
+   
+    let guessNumber = Number(prompt('Guess the Number between 1 and 10'));
+
+    if (guessNumber === randomNumber){
+        alert("You got it right!");
+    }
+
+    else if (guessNumber > randomNumber) {
+       alert ("Your guess was " + guessNumber + ". That's too high. Try Again!");
+       while (guessNumber > randomNumber) {
+        let guessNumber = Number(prompt('Guess the Number between 1 and 10'));
+    }
+    }
+
+    else if (guessNumber < randomNumber) {
+        alert("Your guess was " + guessNumber + ". That's too low. Try Again!")
+    }
+
+   
